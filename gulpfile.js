2b46585604e18c4ee43f6e4f18b092abdf3d1b04
mpodriezov <mpodryezov@gmail.com>
2014-06-15 21:37:13 +0200

    Fixes before push

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,17 @@ var less = require('gulp-less');
 var path = require('path');
 var gulp = require('gulp');
 
+
 gulp.task('less', function () {
-    gulp.src('./src/less/styles.less')
+    return gulp.src('src/less/styles.less')
         .pipe(less({
             paths: [ path.join(__dirname, 'src', 'less') ]
         }))
-        .pipe(gulp.dest('src/css'));
+        .pipe(gulp.dest(path.join(__dirname, 'src', 'css')));
 });
+
+gulp.task('watch', function () {
+    gulp.watch(path.join(__dirname, 'src/less/*.less'), ['less']);
+});
+
+gulp.task('default', ['watch']);
