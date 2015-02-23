e049ea8edda34f7913024e4b32ce43077c30c888
mpodriezov <mpodryezov@gmail.com>
2015-02-23 20:58:19 +0100

    Refactoring projects

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -41,91 +41,126 @@ angular.module('standups.ctrl', ['standups.helpers', 'standups.services'])
 
 
     /* Controller for projects view */
-    .controller('ProjectCtrl', ["$scope", "$h", "Projects", 'Store', function ($scope, $h, Projects) {
+    .controller('ProjectCtrl', ["$scope", "$h", 'Store', function ($scope, $h, Store) {
 
-        //subview
+        //subview: details, edit, list, wizard
         $scope.view = null;
+
+        // wizard like steps for startup
         $scope.wizardStep = 1;
+
+        // temporary stuff, edited projects, not created stuff
         $scope.temp = {};
 
-        $scope.init = function () {
-            Projects.load().then(function () {
-                if (Projects.getActive()) {
-                    $scope.details();
-                } else {
-                    $scope.wizard()
-                }
+        // projects related models
+        $scope.data = {};
 
+        $scope.resetTemp = function () {
+            $scope.temp = {
+                project: {users: []}
+            };
+        };
+
+        $scope.init = function () {
+            $scope.resetTemp();
+            $scope.data.projects = [];
+            Store.get("projects").then(function (projects) {
+                if ($h.isArray(projects)) {
+                    $scope.data.projects = projects;
+                    $scope.data.project = _.findWhere(projects, {active: true});
+                    $scope.details($scope.data.project);
+                } else {
+                    $scope.wizard();
+                }
             });
+        };
+
+        $scope.edit = function (project) {
+            $scope.resetTemp();
+            $scope.temp.project = _.clone(project);
+            $scope.view = "edit";
+        };
+
+        $scope.cancelEdit = function (project) {
+            $scope.resetTemp();
+            $scope.details($scope.data.project);
         };
 
         $scope.wizard = function () {
             $scope.view = "wizard";
         };
 
-        $scope.getProjects = function () {
-            return Projects.getList();
-        };
-
-        $scope.activeProject = function () {
-            return Projects.getActive();
-        };
-
-        $scope.selectProject = function (project) {
-            Projects.setActive(project);
-        };
-
-        $scope.addProject = function (projectName) {
-            projectName = projectName.trim();
-            if (projectName) {
-                var project = Projects.add(projectName);
-                if (!this.activeProject()) {
-                    Projects.setActive(project);
-                }
-                $scope.temp.projectName = "";
-            }
-        };
-
-        $scope.details = function () {
+        $scope.details = function (project) {
+            $scope.data.project = project;
             $scope.view = "details";
-        };
-
-        $scope.addUser = function (userName) {
-            userName = userName.trim();
-            if (userName) {
-                Projects.addUser(Projects.getActive().id, {
-                    id: $h.generateId(),
-                    name: userName
-                });
-                $scope.temp.userName = "";
-            }
-        };
-
-        $scope.removeUser = function ($index) {
-            Projects.removeUser(Projects.getActive().id, $index);
-        };
-
-        $scope.removeProject = function (project) {
-            Projects.remove(project.id);
-        };
-
-        $scope.save = function () {
-            var project = Projects.getActive();
-            Projects.save(project);
-            project.isEdited = false;
         };
 
         $scope.list = function () {
             $scope.view = "list";
         };
 
-        $scope.editProject = function (project) {
-            if (project) {
-                Projects.setActive(project);
-                project.isEdited = !project.isEdited;
-                $scope.view = "details";
+        $scope.select = function (project) {
+            if ($scope.data.project) {
+                $scope.data.project.active = false;
+            }
+            $scope.data.project = project;
+            $scope.data.project.active = true;
+            //todo: save store
+        };
+
+        $scope.update = function (project) {
+            $scope.data.project = project;
+            //todo: save store
+        };
+
+        $scope.create = function (project) {
+            if (project.name && project.name.trim() !== "") {
+                //generate id
+                project.id = $h.generateId();
+                if (!$scope.data.project) {
+                    $scope.data.project = project;
+                    $scope.data.project.active = true;
+                }
+                $scope.data.projects.push(project);
+                $scope.resetTemp();
+
+                //todo: save store
             }
         };
+
+        $scope.addUser = function (project, userName) {
+            userName = userName.trim();
+            if (userName !== "") {
+                project.users.push({
+                    id: $h.generateId(),
+                    name: userName
+                });
+            }
+        };
+
+        $scope.removeUser = function (project, user) {
+            if (project && user) {
+                project.users = _.without(project.users, _.findWhere(project.users, {id: user.id}));
+            }
+        };
+
+        $scope.remove = function (project) {
+            Projects.remove(project.id);
+        };
+
+        $scope.goWizardNext = function () {
+            if ($scope.wizardStep == 1 && $scope.temp.project.name && $scope.temp.project.name.trim() !== "") {
+                $scope.wizardStep = 2;
+            }
+            if ($scope.wizardStep == 2 && $scope.temp.project.users.length) {
+                $scope.create($scope.temp.project);
+                $scope.details($scope.data.project);
+            }
+        }
+
+        $scope.test = function (va) {
+            debugger;
+        }
 
     }])
 
