4549e829de241c063b56ba7829fccbf5cff4f0df
mpodriezov <mpodryezov@gmail.com>
2015-03-08 21:22:20 +0100

    Re-factored main.js and app.js for using with single page application

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,14 +1,1 @@
-$(function () {
-    angular.module('standups', ['ng', 'standups.ctrl', 'standups.directives', 'standups.helpers']);
-    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-        if (request.cmd == "bootstrap") {
-            chrome.extension.sendRequest({ cmd: "read_file", file: "templates/index.html" }, function (html) {
-                var extension = document.getElementById('extension-standups');
-                if (!extension) {
-                    document.body.innerHTML += html;
-                    angular.bootstrap(document.getElementById('extension-standups'), ['standups']);
-                }
-            });
-        }
-    });
-});
+angular.module('standups', ['ng', 'standups.ctrl', 'standups.directives', 'standups.helpers']);
